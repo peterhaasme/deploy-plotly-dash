5c4f087e555cb2f79995eb50addbe0caa66d404f
peter haas <peter@peterhaas.me>
2023-04-11 11:15:31 -0400

    Create plotly application

---
+++ app.py
@@ -0,0 +1,47 @@
+"""Dash App Layout With Figure and Slider
+
+Source: https://dash.plotly.com/basic-callbacks#dash-app-layout-with-figure-and-slider
+"""
+
+# Import packages
+from dash import Dash, dcc, html, Input, Output
+import pandas as pd
+import plotly.express as px
+
+# Read external data into pandas DataFrame
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+
+# Initialize the app
+app = Dash(__name__)
+
+# App layout
+app.layout = html.Div([
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        df['year'].min(),
+        df['year'].max(),
+        step=None,
+        value=df['year'].min(),
+        marks={str(year): str(year) for year in df['year'].unique()},
+        id='year-slider'
+    )
+])
+
+# App callbacks
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    Input('year-slider', 'value'))
+def update_figure(selected_year):
+    filtered_df = df[df.year == selected_year]
+
+    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
+                     size="pop", color="continent", hover_name="country",
+                     log_x=True, size_max=55)
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
+
+# Run the app
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
