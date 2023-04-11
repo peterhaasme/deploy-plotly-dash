50232a2a63a9b2a4aba1437c66817fba17060700
peter haas <peter@peterhaas.me>
2023-04-11 11:15:39 -0400

    Create wsgi entry point

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+"""wsgi entry point
+
+This module serves as the entry point for our application. It defines the behaviour
+of the Gunicorn server with our application.
+"""
+
+from app import app
+if __name__ == '__main__':
+    app.run_server()
